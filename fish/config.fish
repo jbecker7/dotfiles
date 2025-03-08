ede355ce6e019ef2eae08667e08407355ef24073
Jonathan Becker <flopanop1@gmail.com>
2025-03-08 08:58:43 -0500

    Cleaned up fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,65 +1,41 @@
 fish_config theme choose "Dracula Official"
-set -gx PATH /opt/homebrew/Cellar/postgresql@15/15.12/bin/psql $PATH
-alias neofetch='neofetch --ascii  /Users/jcb/reimu.txt --ascii_colors 4'
-set -gx PATH /usr/local/texlive/2024basic/bin/universal-darwin $PATH
+
+# PATH cleanup: Consolidated into one block
+set -gx PATH /opt/homebrew/Cellar/postgresql@15/15.12/bin/psql \
+             /usr/local/texlive/2024basic/bin/universal-darwin \
+             /Users/jcb/Library/Python/3.9/bin \
+             /usr/local/opt/python/libexec/bin \
+             /Library/Frameworks/Python.framework/Versions/3.11/bin \
+             /Applications/Racket\ v8.14/bin \
+             /Users/jcb/.local/bin \
+             $PATH
+
+# Fish user paths 
 set -U fish_user_paths /usr/local/texlive/2024basic/bin/universal-darwin $fish_user_paths
 
+# Interactive session settings
 if status is-interactive
     # Commands to run in interactive sessions can go here
-set -gx PATH /Users/jcb/Library/Python/3.9/bin $PATH
-
-
 end
-set -gx PATH /usr/local/opt/python/libexec/bin $PATH
 
-
-# Setting PATH for Python 3.11
-# The original version is saved in /Users/jcb/.config/fish/config.fish.pysave
-set -x PATH "/Library/Frameworks/Python.framework/Versions/3.11/bin" "$PATH"
-
+# Function: Apple Music CLI Player (very nice CLI tool haha)
 function amplayer
     /Applications/Apple-Music-CLI-Player-main/src/am.sh $argv
 end
 
-# alias lvim "/Users/jcb/.local/bin/lvim"
-
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
+# Function: Run Bash Script
 function run_bash_script
-    # Check if the first argument is provided and if the file exists
-    if test -z "$argv[1]"
-        echo "Please provide a script path."
-        return 1
-    else if not test -f $argv[1]
+    if not test -f $argv[1]
         echo "Script file does not exist: $argv[1]"
         return 1
     end
-
-    # Store the script path
-    set script_path $argv[1]
-
-    # Remove the script path from the arguments list
-    set -e argv[1]
-
-    # Execute the script with the remaining arguments
-    bash $script_path $argv
+    bash $argv
 end
-set -gx PATH /Applications/Racket\ v8.14/bin $PATH
 
-
-
+# Init tools
 zoxide init fish | source
-
-
-# neofetch
-
 starship init fish | source
 
+# Disable mail check
 set -U MAILCHECK 0
 
-
-
-
-# Created by `pipx` on 2025-03-06 14:42:12
-set PATH $PATH /Users/jcb/.local/bin
